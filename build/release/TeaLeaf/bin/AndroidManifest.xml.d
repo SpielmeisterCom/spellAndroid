dc10502d88d37dd4e886c24928ded74f79052f7c
Julian Haupt <jh@spielmeister.com>
2013-07-14 21:48:12 +0200

    removed build directory

--- build/release/TeaLeaf/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/julian/workspace/spellEngine/modules/spellAndroid/modules/native-android/TeaLeaf/bin/AndroidManifest.xml : \
-/home/julian/workspace/spellEngine/modules/spellAndroid/modules/native-android/TeaLeaf/AndroidManifest.xml \
