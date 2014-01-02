41379d3579c5cf0ba964a09258bfe15ba8a7f625
Julian Haupt <jh@spielmeister.com>
2014-01-03 00:31:28 +0100

    made android_sdk & android_ndk path configurable

--- setup_env.sh
+++ setup_env.sh
@@ -1,33 +1,20 @@
 #!/bin/sh
 
-NDK_ROOT=/opt/android-ndk
-SDK_ROOT=/opt/android-sdk
-UNAME=$(uname -sm)
-
-if [ "$UNAME" = "Linux i386" ]; then
-	echo "Your arch is not supported!"
-	exit 1
-
-elif [ "$UNAME" = "Linux x86_64" ]; then
-	ARCH="linux-x64"
-
-elif [ "$UNAME" = "Darwin x86_64" ]; then
-	ARCH="osx-x64"
-fi
-
 if [ "$1" = "debug" ]; then
 	MODE=DEBUG
 fi
 
-if [ ! -d /opt/android-ndk ]; then
-	echo "The android ndk was not found in $NDK_ROOT"
+if [ ! -x "$ANDROID_NDK_PATH"/ndk-build ]; then
+	echo "The android ndk was not found in $ANDROID_NDK_PATH"
+	echo "Please set your ANDROID_NDK_PATH environment vairable to a valid android ndk"
 	echo "You need at least version r9 and can download it under"
 	echo "http://developer.android.com/tools/sdk/ndk/index.html"
 	exit 1;
 fi
 
-if [ ! -d /opt/android-sdk ]; then
-	echo "The android sdk was not found in $SDK_ROOT"
+if [ ! -x "$ANDROID_SDK_PATH"/tools/android ]; then
+	echo "The android sdk was not found in $ANDROID_SDK_PATH"
+	echo "Please set your ANDROID_SDK_PATH environment vairable to a valid android sdk"
 	echo "You need at least version 22.2.1 and can download it under"
 	echo "http://developer.android.com/sdk/index.html"
 	echo "You can find the SDK Tools Only version under DOWNLOAD FOR OTHER PLATFORMS"
@@ -35,15 +22,13 @@ if [ ! -d /opt/android-sdk ]; then
 	exit 1;
 fi
 
-#path to javac
-
 if [ ! -x "$JAVA_HOME"/bin/java -o ! -x "$JAVA_HOME"/bin/javac ]; then 
 	echo "The JAVA_HOME environment variable is not defined correctly"
 	echo "Please set your JAVA_HOME variable to a valid JDK"	
 	exit 1
 fi
 
-export PATH=$PATH:$SDK_ROOT/platform-tools:$SDK_ROOT/tools:$NDK_ROOT
+export PATH=$PATH:$ANDROID_SDK_PATH/platform-tools:$ANDROID_SDK_PATH/tools:$ANDROID_NDK_PATH
 
 echo Enviroment setup:
 echo JAVA_HOME: $JAVA_HOME
